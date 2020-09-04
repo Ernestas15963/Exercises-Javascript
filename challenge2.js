c542d570a965f719131ab415e15a1bb5c240d05a
Ernestas15963 <66821838+Ernestas15963@users.noreply.github.com>
2020-09-04 16:33:30 +0300

    Update challenge2.js

--- challenge2.js
+++ challenge2.js
@@ -5,8 +5,8 @@
 
 
 function generateCat(){
-    var image = document.createElement('img');
-    var div = document.getElementById('flex-cat-gen');
+    let image = document.createElement('img');
+    let div = document.getElementById('flex-cat-gen');
     image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
     div.appendChild(image);
 
