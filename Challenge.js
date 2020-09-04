e1dd5fe391310ad60cdebcc665efb08b59a069ea
Ernestas15963 <66821838+Ernestas15963@users.noreply.github.com>
2020-09-04 16:35:16 +0300

    Update Challenge.js

--- Challenge.js
+++ Challenge.js
@@ -4,10 +4,10 @@
 
 
 function ageInDays(){
-    var birthYear = prompt('What year you born...Good friend?');
-    var ageInDaysz = (2018 - birthYear)* 365;
-    var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are ' + ageInDaysz + ' days old.')
+    let birthYear = prompt('What year you born...Good friend?');
+    let ageInDaysz = (2018 - birthYear)* 365;
+    let h1 = document.createElement('h1');
+    let textAnswer = document.createTextNode('You are ' + ageInDaysz + ' days old.')
     h1.setAttribute('id', 'ageInDays');
     h1.appendChild(textAnswer);
     document.getElementById('flex-box-result').appendChild(h1);
