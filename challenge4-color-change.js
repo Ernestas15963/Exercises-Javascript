fe30e81355b81786f1caa61320b85da485f8cf2f
Ernestas15963 <66821838+Ernestas15963@users.noreply.github.com>
2020-09-04 17:40:15 +0300

    Update challenge4-color-change.js

--- challenge4-color-change.js
+++ challenge4-color-change.js
@@ -59,7 +59,7 @@ function randomColors(){
     let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
 
     for (let i = 0; i < all_buttons.length; i++) {
-        var randomNumber = Math.floor(Math.random() * 4);
+        let randomNumber = Math.floor(Math.random() * 4);
         all_buttons[i].classList.remove(all_buttons[i].classList[1]);
         all_buttons[i].classList.add(choices[randomNumber]);
     }
